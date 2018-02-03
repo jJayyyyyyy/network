7253b37a3806570e1a416c0718bc880e11fde9d4
FrozenMap <ljjdbd123@hotmail.com>
2018-02-03 15:06:11 +0000

    implement FizzBuzz with vuejs directly in the frontend

---
+++ vuejs/demo/fizzbuzz/app.js
@@ -0,0 +1,30 @@
+var app = new Vue({
+	el: '#app',
+	data: {
+		num: null,
+		FB: []
+	},
+	methods: {
+		getFB: function(){
+			var FB = []
+			var fb = ''
+			var len = Number(this.num)
+			for(let i = 1; i <= len; i++){
+				if (i % 15 === 0){
+					fb = 'FizzBuzz'
+				}else if(i % 3 === 0){
+					fb = 'Fizz'
+				}else if(i % 5 === 0){
+					fb = 'Buzz'
+				}else{
+					fb = i
+				}
+				console.log(fb)
+				FB.push(fb)
+			}
+			console.log(FB)
+			this.FB = FB
+			this.show = !this.show
+		}
+	}
+})
---
+++ vuejs/demo/fizzbuzz/readme.md
@@ -0,0 +1,11 @@
+##  FizzBuzz
+
+Udacity cs253介绍的一个小程序，就是输入一个正整数n，输出一个从1到n的序列，如果index能被3整除，它的值就是Fizz，如果能被5整除就是Buzz，如果能同时被3和5整除，那就输出FizzBuzz
+
+之前在[这里](https://github.com/jJayyyyyyy/network/tree/master/udacity/cs253)的实现方式是，在client输入，提交给server，服务器算好一个list之后，render到一个html，再返回这个页面
+
+这个demo就是把这一来一回给省略了，直接在前端完成计算和render
+
+##  TODO
+
+*   transition过渡效果
