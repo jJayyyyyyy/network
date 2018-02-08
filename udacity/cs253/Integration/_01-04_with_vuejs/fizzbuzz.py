542a8a5f2960c9f1d1a1a658fa4cb965986f9fee
FrozenMap <ljjdbd123@hotmail.com>
2018-02-08 14:09:46 +0000

    separate frontend and backend, partly

--- udacity/cs253/Integration/_01-04_with_vuejs/fizzbuzz.py
+++ udacity/cs253/Integration/_01-04_with_vuejs/fizzbuzz.py
@@ -1,10 +1,6 @@
 from page import Page
 
 class FizzBuzzHandler(Page):
-	filename = './templates/fizzbuzz.html'
+	filename = 'fizzbuzz.html'
 	def get(self):
-		with open(self.filename, 'r') as f:
-			return f.read()
-		# fizzbuzz_strn = self.get_args('fizzbuzz_strn')
-		# fizzbuzz_list = get_fizzbuzz_list(fizzbuzz_strn)
-		# return self.render(self.filename, fizzbuzz_list=fizzbuzz_list)
+		return self.render_raw(self.filename)
--- udacity/cs253/Integration/_01-04_with_vuejs/rot13.py
+++ udacity/cs253/Integration/_01-04_with_vuejs/rot13.py
@@ -1,13 +1,6 @@
 from page import Page
-import codecs
 
 class ROT13Handler(Page):
 	filename = 'rot13.html'
 	def get(self):
-		return self.render(self.filename)
-
-	def post(self):
-		rot13_text = self.form().get('rot13_text')
-		if rot13_text:
-			rot13_text = codecs.encode(rot13_text, ('rot13'))
-		return self.render(self.filename, rot13_text=rot13_text)
+		return self.render_raw(self.filename)
