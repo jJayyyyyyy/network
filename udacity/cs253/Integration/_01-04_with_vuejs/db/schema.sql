e3ac18d7139ff546ae0dd3ce2004d2a00066393c
FrozenMap <ljjdbd123@hotmail.com>
2018-02-11 13:43:27 +0000

    update db schema

--- udacity/cs253/Integration/_01-04_with_vuejs/db/schema.sql
+++ udacity/cs253/Integration/_01-04_with_vuejs/db/schema.sql
@@ -3,18 +3,18 @@ drop table if exists artworks;
 drop table if exists users;
 
 create table posts (
-	post_id integer primary key autoincrement,
+	id integer primary key autoincrement,
 	update_date date not null,
 	subject text not null,
 	content text not null);
 
 create table artworks (
-	artwork_id integer primary key autoincrement,
+	id integer primary key autoincrement,
 	subject text not null,
 	content text not null);
 
 create table users (
-	user_id integer primary key autoincrement,
+	id integer primary key autoincrement,
 	username text not null unique,
 	pw_hash text not null,
 	email text);
